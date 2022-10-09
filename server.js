a0c9292fcff525de3f9929783545727e8ab8244e
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-10-08 21:16:58 -0500

    Completed Project 1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express, { application } from 'express';
+import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //Initialize Express application
 const bookstoreApp = express();
@@ -15,10 +16,23 @@ bookstoreApp.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
-bookstoreApp.post('/api/patrons', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+bookstoreApp.post(
+    '/api/patrons', 
+    [
+        check('date', 'Please enter the date for today.').not().isEmpty(),
+        check('name', 'Please enter your first and last name.').not().isEmpty(),
+        check('email', 'Please enter a valid email.').isEmail(),
+        check('bookTitle', 'Please enter the title of the book.').not().isEmpty(),
+        check('bookAuthor', 'Please enter the author of the book.').not().isEmpty()
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        } else {
+            return res.send(req.body);
+        }
+    });
 
 //Add Connection Listener
 const port = 5000;
